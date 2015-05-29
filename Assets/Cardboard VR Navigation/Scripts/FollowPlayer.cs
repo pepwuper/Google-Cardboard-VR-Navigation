bd25f258cc819d8c5683ec9f4d75c23659faf2c9
theBrandonWu <brandon.wu.com@gmail.com>
2015-05-29 13:51:25 +0100

    Initial commit

---
+++ Assets/Cardboard VR Navigation/Scripts/FollowPlayer.cs
@@ -0,0 +1,16 @@
+﻿/* © 2015 Studio Pepwuper http://www.pepwuper.com */
+
+using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+	public GameObject Player;
+	private Vector3 playerPosition;
+	
+	void LateUpdate () {
+		playerPosition = Player.transform.position;
+		Vector3 newPos = new Vector3(playerPosition.x, transform.position.y, playerPosition.z); // not changing Y value
+		transform.position = newPos;
+	}
+}
---
+++ Assets/Cardboard VR Navigation/Scripts/PlayerMotor.cs
@@ -0,0 +1,27 @@
+﻿/* © 2015 Studio Pepwuper http://www.pepwuper.com */
+
+using UnityEngine;
+using System.Collections;
+
+public class PlayerMotor : MonoBehaviour {
+
+	public GameObject Cursor; // Google Cardboard SDK: Cursor / GazePointer from CardboardMain Prefab
+	private Vector3 goal;
+	private NavMeshAgent agent;
+	
+	void Start() {
+		this.agent = GetComponent<NavMeshAgent>();
+		this.goal = new Vector3(0f, 0f, 0f);
+	}
+	
+	//Set navigation destination to the position of the cursor
+	//Ex. Call this from an event trigger on the floor object
+	public void SetDestinationToCursor() {
+		this.goal = Cursor.transform.position;
+		MoveToDestination();
+	}
+	
+	void MoveToDestination(){
+		this.agent.destination = goal; 
+	}
+}
